c538a3e468d6a4f2cec911f1dfe68735ecd1bdb2
thais <thaisinea.tops@gmail.com>
2025-07-28 20:27:55 -0300

    tipos de funções

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-let corSite = "azul";
+/*let corSite = "azul";
 
 function resetCor(cor){
     corSite = cor;
@@ -7,3 +7,18 @@ function resetCor(cor){
 console.log(corSite);
 resetCor("Vermelho");
 console.log(corSite);
+*/
+
+//Função onde não devolve resultado
+function nomes(){
+    console.log('Kammylleh');
+}
+//chamei a função
+nomes();
+
+//faz calculo e retorna valor
+function multiplicar(valor){
+    return valor*2;
+}
+let result = multiplicar(10);
+console.log(result);
